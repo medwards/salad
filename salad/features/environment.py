75360d6e0f995f5f616854bc1aa4a85b6b05c4e7
Michael Edwards <medwards@walledcity.ca>
2013-02-04 01:51:24 +0100

    Use behave for salad feature test

--- salad/features/environment.py
+++ salad/features/environment.py
@@ -8,37 +8,46 @@ from sys import path
 SALAD_ROOT = abspath(join(dirname(__file__), "../", "../"))
 path.insert(0, SALAD_ROOT)
 
-from lettuce import before, world, after
 from salad.tests import TEST_SERVER_PORT
-from salad.terrains.everything import *
 from salad.logger import logger
 
 
-@before.all
-def setup_subprocesses():
-    world.subprocesses = []
+def before_all(context):
+    create_tempfile(context)
+    setup_subprocesses(context)
+    setup_test_server(context)
 
 
-@before.all
-def setup_test_server():
+def after_all(context):
+    teardown_test_server(context)
+    remove_tempfile()
+
+def setup_subprocesses(context):
+    context.subprocesses = []
+
+
+def setup_test_server(context):
     file_server_command = "python -m SimpleHTTPServer %s" % (TEST_SERVER_PORT)
     test_dir = abspath(join(SALAD_ROOT, "salad", "tests", "html"))
-    world.silent_output = file('/dev/null', 'a+')
-    world.tempfile = file('/dev/null', 'a+')
+    context.silent_output = file('/dev/null', 'a+')
+    context.tempfile = file('/dev/null', 'a+')
 
-    world.subprocesses.append(Popen(file_server_command,
-                                    shell=True,
+    context.subprocesses.append(Popen(file_server_command, shell=True,
                                     cwd=test_dir,
-                                    stderr=world.silent_output,
-                                    stdout=world.silent_output
+                                    stderr=context.silent_output,
+                                    stdout=context.silent_output
                                 ))
     time.sleep(3)  # Wait for server to spin up
 
 
-@after.all
-def teardown_test_server(total):
-    world.silent_output.close()
-    for s in world.subprocesses:
+def create_tempfile(context):
+    context.tempfile = file('/tmp/temp_lettuce_test', 'a+')
+    context.tempfile.close()
+
+
+def teardown_test_server(context):
+    context.silent_output.close()
+    for s in context.subprocesses:
         try:
             s.terminate()
         except:
@@ -49,12 +58,5 @@ def teardown_test_server(total):
                 pass
 
 
-@before.all
-def create_tempfile():
-    world.tempfile = file('/tmp/temp_lettuce_test', 'a+')
-    world.tempfile.close()
-
-
-@after.all
-def remove_tempfile(total):
+def remove_tempfile():
     remove("/tmp/temp_lettuce_test")
